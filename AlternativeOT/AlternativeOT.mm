6e135435dcd24c7eac513c86c0bbdfac1bb8b754
rredcat <rredcat@aef2b62d-4553-0410-b098-a72ca8ef7944>
2008-11-04 10:21:25 +0000

    Add todo list.

---
+++ AlternativeOT/AlternativeOT.mm
@@ -0,0 +1,57 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1225749504406" ID="Freemind_Link_1291210681" MODIFIED="1225751024109" TEXT="AlternativeOT">
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1225749595453" ID="_" MODIFIED="1225751024109" POSITION="right" TEXT="View">
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1225749637156" ID="Freemind_Link_1221286275" MODIFIED="1225751024109" TEXT="Descriptions">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1225749644281" ID="Freemind_Link_1943231417" MODIFIED="1225751024109" TEXT="Notes">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1225749660203" ID="Freemind_Link_1234598450" MODIFIED="1225751024109" TEXT="Replication Propcedures">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1225749684125" ID="Freemind_Link_1497099648" MODIFIED="1225751024109" TEXT="Atachment">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1225749695859" ID="Freemind_Link_166537657" MODIFIED="1225751024109" TEXT="History">
+<icon BUILTIN="button_cancel"/>
+</node>
+</node>
+<node CREATED="1225749602859" FOLDED="true" ID="Freemind_Link_92320487" MODIFIED="1225751024109" POSITION="left" TEXT="Edit">
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1225749792109" ID="Freemind_Link_309887620" MODIFIED="1225751024109" TEXT="Notes">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1225749809390" ID="Freemind_Link_1486284367" MODIFIED="1225751024109" TEXT="Atachment">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1225750230062" ID="Freemind_Link_1760651669" MODIFIED="1225751024109" TEXT="AssignedTo">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1225750242531" ID="Freemind_Link_277954626" MODIFIED="1225751024109" TEXT="Status">
+<icon BUILTIN="button_cancel"/>
+</node>
+</node>
+<node CREATED="1225750940250" FOLDED="true" ID="Freemind_Link_1388547770" MODIFIED="1225751024093" POSITION="right" TEXT="List">
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1225750976750" ID="Freemind_Link_1564462608" MODIFIED="1225751024093" TEXT="DefectId">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1225750983000" ID="Freemind_Link_1140812249" MODIFIED="1225751024093" TEXT="Name">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1225750986578" ID="Freemind_Link_75809997" MODIFIED="1225751031890" TEXT="AssignedTo">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1225751009671" ID="Freemind_Link_509295543" MODIFIED="1225751024093" TEXT="Priority">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1225751015718" ID="Freemind_Link_1422330852" MODIFIED="1225751024093" TEXT="Status">
+<icon BUILTIN="button_cancel"/>
+</node>
+</node>
+</node>
+</map>
