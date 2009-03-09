772ad10fb3f4dfdc982ed9b93bcef68ff2cdcb8c
rredcat <rredcat@aef2b62d-4553-0410-b098-a72ca8ef7944>
2009-03-09 22:44:36 +0000

    MagicWizard is started.

---
+++ MagicWizard/MagicWizard/Implementations/MagicWizard.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Miracle
+{
+	public class MagicWizard : Window
+	{
+		#region Initializaition
+		/// <summary>
+		/// Initializes the <see cref="MagicWizard"/> class.
+		/// </summary>
+		static MagicWizard()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata( typeof( MagicWizard )
+				, new FrameworkPropertyMetadata( typeof( MagicWizard ) ) );
+		}
+		public MagicWizard()
+		{
+			AllowsTransparency = true;
+			WindowStyle = WindowStyle.None;
+			WindowStartupLocation = WindowStartupLocation.CenterScreen;
+			MouseLeftButtonDown += new MouseButtonEventHandler( OnMouseLeftButtonDown );
+		}
+		#endregion
+
+		#region Implementation
+		private void OnMouseLeftButtonDown( object sender, MouseButtonEventArgs e )
+		{
+			DragMove();
+		}
+		#endregion
+	}
+}
---
+++ MagicWizard/MagicWizardSample/Window1.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using Miracle;
+
+namespace MagicWizardSample
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Window1
+	{
+		#region Initialization
+		public Window1()
+		{
+			InitializeComponent();
+		}
+		#endregion
+
+		#region Implementation
+		private void OnClick( object sender, RoutedEventArgs e )
+		{
+			MagicWizard wiz = new MagicWizard();
+			wiz.Show();
+		}
+		#endregion
+	}
+}
